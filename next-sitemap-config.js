3dd847fab3c8d09abf5636c71fdcaef796731399
Akromjon <152626511+RustamovAkrom@users.noreply.github.com>
2025-09-01 20:58:06 +0500

    add configure next sitemap

--- next-sitemap-config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: 'https://akrom-omega.vercel.app',
-  generateRobotsTxt: true, // автоматически создаст robots.txt
-  changefreq: 'daily',
-  priority: 0.7,
-  sitemapSize: 5000,
-};
