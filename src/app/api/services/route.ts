64f645beb56a306d4dca9efe3662469dcd02d1e9
Akromjon <152626511+RustamovAkrom@users.noreply.github.com>
2025-09-19 01:40:35 +0500

    change error of services

--- src/app/api/services/route.ts
+++ src/app/api/services/route.ts
@@ -3,16 +3,8 @@ import fs from "fs";
 import path from "path";
 
 export async function GET() {
-  try {
-    const filePath = path.join(process.cwd(), "data", "services.json");
-    const jsonData = fs.readFileSync(filePath, "utf-8");
-    const data = JSON.parse(jsonData);
-
-    return NextResponse.json(data);
-  } catch (error) {
-    return NextResponse.json(
-      { error: "Failed to load services" },
-      { status: 500 }
-    );
-  }
+  const filePath = path.join(process.cwd(), "data", "services.json");
+  const jsonData = fs.readFileSync(filePath, "utf-8");
+  const data = JSON.parse(jsonData);
+  return NextResponse.json(data);
 }
