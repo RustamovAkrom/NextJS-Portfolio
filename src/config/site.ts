d174119478c4326df00743747f0dae28f253907f
Akromjon <152626511+RustamovAkrom@users.noreply.github.com>
2025-09-21 18:14:49 +0500

    add 4 portfolio

--- src/config/site.ts
+++ src/config/site.ts
@@ -13,6 +13,7 @@ export const siteConfig = {
     { label: "Skills", href: "/skills" },
     { label: "Portfolio", href: "/projects" },
     { label: "Services", href: "/services" },
+    { label: "Blog", href: "https://akromblogdev.vercel.app" },
     { label: "Contact", href: "/contact" },
   ],
 
